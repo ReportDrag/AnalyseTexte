eecf6ac999ebf4ad5fdc9bf2ab88e67f565d1090
ReportDrag <oberniguet@gmail.com>
2022-03-09 16:34:40 +0100

    AnalyseTexte

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let longueur = "";
+let Nbmot = 0;
+let Nbe = 0;
+let RepetLettre = 0;
+
+const analyser = () =>{
+    console.log('Analyser');
+
+    let div = document.createElement('div');
+    div.innerHTML="Salut";
+    document.getElementById('div').appendChild(div);
+
+}
